77841e2cfa924f621f52c4447c085233c9abcdb3
AlexEvs <evstafiadi.shura@yandex.ru>
2023-02-12 19:38:13 +0300

    SCSS, tsconfig, photo, roboto font

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import photo from 'src/assets/images/photo.jpg';
+import 'src/App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="app-header">
+        <img className={"app-photo"} src={photo}/>
+        <div>Common info</div>
       </header>
+      <div>Tabs</div>
+      <footer>Footer</footer>
     </div>
   );
 }
