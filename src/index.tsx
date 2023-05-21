803906a664321024ae86debe3b16dea0ec76bde2
AlexEvs <evstafiadi.shura@yandex.ru>
2023-05-21 19:44:02 +0300

    Added canvas in Matrix style (JS)

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import 'src/index.css';
 import App from 'src/App';
+import { drawMatrixCanvas } from 'src/MatrixCanvas';
 import reportWebVitals from 'src/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
@@ -16,3 +17,4 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+drawMatrixCanvas();
