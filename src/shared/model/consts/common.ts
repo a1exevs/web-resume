7e7027e6a74c9de122b47e7683c35b2a1322be5a
AlexEvs <aleksandrevstafiadi@gmail.com>
2025-10-05 21:25:59 +0300

    [Common] Story Book configuration and stories for all pages

---
+++ src/shared/model/consts/common.ts
@@ -0,0 +1,11 @@
+export const XS_PX = 0;
+export const SM_PX = 600;
+export const MD_PX = 900;
+export const LG_PX = 1200;
+export const XL_PX = 1536;
+
+export const DESKTOP_4K_SCREEN_WIDTH_PX = 3840;
+export const DESKTOP_SCREEN_WIDTH_PX = 1920;
+export const TABLET_SCREEN_WIDTH_PX = 960;
+export const MOBILE_SCREEN_WIDTH_PX = 375;
+export const APP_MIN_WIDTH = 360;
---
+++ src/shared/model/index.ts
@@ -0,0 +1,8 @@
+// consts
+export {
+  DESKTOP_4K_SCREEN_WIDTH_PX,
+  TABLET_SCREEN_WIDTH_PX,
+  DESKTOP_SCREEN_WIDTH_PX,
+  MOBILE_SCREEN_WIDTH_PX,
+  APP_MIN_WIDTH,
+} from 'src/shared/model/consts/common';
