2f85c418142f1d5c07c6a9d6abc47ca2fbb014d8
AlexEvs <aleksandrevstafiadi@gmail.com>
2025-11-04 18:42:55 +0300

    upd: capitalize fix

--- src/pages/skills-page/ui/continuous-learning-card/continuous-learning-card.tsx
+++ src/pages/skills-page/ui/continuous-learning-card/continuous-learning-card.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import classes from 'src/pages/skills-page/ui/continuous-learning-card/continuous-learning-card.module.scss';
-import { currentLang, Icon, toUnderscore } from 'src/shared';
+import { capitalizeLabel, currentLang, Icon, toUnderscore } from 'src/shared';
 import { IconName } from 'src/shared/ui/icon/icon.names';
 
 const ContinuousLearningCard: React.FC = () => (
   <div className={classes.ContinuousLearningCard}>
     <Icon width="5rem" icon={IconName.PSYCHOLOGY} className={classes.ContinuousLearningCard__Icon} />
-    <h3 className={classes.ContinuousLearningCard__Title}>{toUnderscore(currentLang.labels.CONTINUOUS_LEARNING)}</h3>
+    <h3 className={classes.ContinuousLearningCard__Title}>
+      {toUnderscore(capitalizeLabel(currentLang.labels.CONTINUOUS_LEARNING))}
+    </h3>
     <p className={classes.ContinuousLearningCard__Text}>{currentLang.messages.CONSTANTLY_SKILLS_UPGRADING_MESSAGE}</p>
   </div>
 );
