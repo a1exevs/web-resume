5222df2b58b7256b031cf1fef1d12761f657821b
AlexEvs <aleksandrevstafiadi@gmail.com>
2025-12-03 20:19:22 +0300

    [65][Feature] Experience page layout improvement

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,29 +1,11 @@
-import Block from 'src/shared/ui/block/block';
 import Card from 'src/shared/ui/card/card';
-import Chip from 'src/shared/ui/chip/chip';
-import Chips from 'src/shared/ui/chips/chips';
 import Field from 'src/shared/ui/field/field';
 import GlitchText from 'src/shared/ui/glitch-text/glitch-text';
 import Icon from 'src/shared/ui/icon/icon';
-import RecordsList from 'src/shared/ui/records-list/records-list';
 import ScanLine from 'src/shared/ui/scan-line/scan-line';
 import SimpleList from 'src/shared/ui/simple-list/simple-list';
 import Stepper from 'src/shared/ui/stepper/stepper';
 import Tabs from 'src/shared/ui/tabs/tabs';
 import TypeWriter from 'src/shared/ui/type-writer/type-writer';
 
-export {
-  Icon,
-  ScanLine,
-  TypeWriter,
-  Block,
-  Card,
-  Chips,
-  Tabs,
-  Chip,
-  Stepper,
-  RecordsList,
-  SimpleList,
-  Field,
-  GlitchText,
-};
+export { Icon, ScanLine, TypeWriter, Card, Tabs, Stepper, SimpleList, Field, GlitchText };
