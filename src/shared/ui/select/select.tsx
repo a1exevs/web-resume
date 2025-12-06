ac886ed0928688b4df90f59057497c997391346b
AlexEvs <aleksandrevstafiadi@gmail.com>
2025-12-06 11:41:38 +0300

    [64][Feature]Language switcher (ru, en)

--- src/shared/ui/select/select.tsx
+++ src/shared/ui/select/select.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import cn from 'classnames';
+import React, { ChangeEvent, FocusEvent, KeyboardEvent, MouseEvent, useState } from 'react';
 
 import classes from 'src/shared/ui/select//select.module.scss';
 import { Option } from 'src/shared/ui/select/select.types';
@@ -12,11 +13,36 @@ type Props = {
 };
 
 const Select: React.FC<Props> = ({ value, onChange, options, className = '', disabled = false }) => {
+  const [open, setOpen] = useState<boolean>(false);
+
+  const handleChange = (e: ChangeEvent<HTMLSelectElement>): void => {
+    onChange(e.target.value);
+    setOpen(false);
+  };
+
+  const handleMouseDown = (_: MouseEvent<HTMLSelectElement>): void => {
+    setOpen(true);
+  };
+
+  const handleBlur = (_: FocusEvent<HTMLSelectElement>): void => {
+    setOpen(false);
+  };
+
+  const handleKeyDown = (_: KeyboardEvent<HTMLSelectElement>): void => {
+    setOpen(true);
+  };
+
   return (
     <select
-      className={`${classes.Select} ${className}`}
+      className={cn(classes.Select, className, {
+        [classes.Select_open]: open,
+      })}
       value={value}
-      onChange={e => onChange(e.target.value)}
+      onMouseDown={handleMouseDown}
+      onKeyDown={handleKeyDown}
+      onBlur={handleBlur}
+      onChange={handleChange}
+      onSelect={handleChange}
       disabled={disabled}
     >
       {options.map(opt => (
