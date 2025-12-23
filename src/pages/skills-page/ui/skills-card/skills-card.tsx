040945582dc8328b15f07633370532d8959574db
Alexander Evstafiadi <58134475+a1exevs@users.noreply.github.com>
2025-12-23 21:11:18 +0300

    [87][Bugfix][UI/UX Designer Audit][Skills page] Continuous Learning card styles (#99)

--- src/pages/skills-page/ui/skills-card/skills-card.tsx
+++ src/pages/skills-page/ui/skills-card/skills-card.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import classes from 'src/pages/skills-page/ui/skills-card/skills-card.module.scss';
+import { Icon, IconName } from 'src/shared';
 
 type Props = {
   title: string;
   items: string[];
+  icon?: IconName;
 };
 
-const SkillsCard: React.FC<Props> = ({ title, items }) => (
+const SkillsCard: React.FC<Props> = ({ title, items, icon }) => (
   <div className={classes.SkillsCard}>
     <h3 className={classes.SkillsCard__Title}>
       <span className={classes.SkillsCard__TitleIcon}>&gt;</span> {title}
@@ -19,6 +21,7 @@ const SkillsCard: React.FC<Props> = ({ title, items }) => (
         </li>
       ))}
     </ul>
+    {icon && <Icon width="5rem" icon={IconName.PSYCHOLOGY} className={classes.SkillsCard__Icon} />}
   </div>
 );
 
